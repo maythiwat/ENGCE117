25ba959e72cd9d4ef21a7d8a4fb1211afdbd3328
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2024-12-01 21:16:47 +0700

    fix: handle invalid matrix input

--- lab/lab1/lab1-6.cpp
+++ lab/lab1/lab1-6.cpp
@@ -6,17 +6,26 @@ int main() {
     int *data, m, n ;
     data = GetMatrix( &m, &n ) ;
 
+    // null = incorrect number
+    if ( data == NULL ) {
+        printf( "Error: Invalid matrix input" ) ;
+        return 0 ;
+    }//end if
+
     // print out matrix
     printf( "Enter number of rows and columns: %d %d \n", m, n ) ;
-    printf( "Enter matrix elements: \n" ) ;
-    for( int i = 0 ; i < m ; i++ ) {
-        for( int j = 0 ; j < n ; j++ ) {
-            printf( "%d ", data[ i * n + j ] ) ;
-        }//end for
 
-        // line break on new row
-        printf("\n") ;
-    }//end for
+    if ( m && n ) {
+        printf( "Enter matrix elements: \n" ) ;
+        for( int i = 0 ; i < m ; i++ ) {
+            for( int j = 0 ; j < n ; j++ ) {
+                printf( "%d ", data[ i * n + j ] ) ;
+            }//end for
+
+            // line break on new row
+            printf("\n") ;
+        }//end for
+    }//end if
 
     return 0 ;
 }//end function
@@ -29,8 +38,27 @@ int* GetMatrix( int *row, int *col ) {
     int *tmp ;
     tmp = new int[ size ] ;
 
-    for( int i = 0 ; i < size ; i++ ) {
-        scanf( "%d", &tmp[ i ] ) ;
+    for ( int i = 0 ; i < ( *row ) ; i++ ) {
+        if (tmp == NULL) {
+            break ;
+        }//end if
+
+        for ( int j = 0 ; j < ( *col ) ; j++ ) {
+            int idx = i * ( *col ) + j ;
+
+            scanf( "%d", &tmp[ idx ] ) ;
+
+            // handle line break
+            if ( getchar() == '\n' ) {
+                // if is last row AND current index is not end
+                if ( i == ( *row - 1 ) && idx < ( size - 1 ) ) {
+                    // dirty way to told is it error or not
+                    tmp = NULL ;
+                }//end if
+
+                break ;
+            }//end if
+        }//end for
     }//end for
 
     return tmp ;
